a59c16c0a6ecd4e2982fdbfbc2a974d1c484d3b4
Jason Schrader <whoabuddy@users.noreply.github.com>
2024-11-07 16:24:29 -0700

    fix: pretty formatting, add assets endpoint

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,13 @@ app.get("/api/v1/extended", async (c) => {
     return new Response(response.body, response);
   } catch (error) {
     return c.json(
-      { error: error instanceof Error ? error.message : `Unknown error ${String(error)}`, status: 500 },
+      {
+        error:
+          error instanceof Error
+            ? error.message
+            : `Unknown error ${String(error)}`,
+        status: 500,
+      },
       500
     );
   }
@@ -30,7 +36,34 @@ app.get("/api/v2/info", async (c) => {
     return new Response(response.body, response);
   } catch (error) {
     return c.json(
-      { error: error instanceof Error ? error.message : `Unknown error ${String(error)}`, status: 500 },
+      {
+        error:
+          error instanceof Error
+            ? error.message
+            : `Unknown error ${String(error)}`,
+        status: 500,
+      },
+      500
+    );
+  }
+});
+
+// https://api.hiro.so/extended/v1/address/{principal}/assets
+app.get("/api/v1/extended/address/:principal/assets", async (c) => {
+  try {
+    const id = c.env.HIRO_API.idFromName("hiro-api");
+    const hiroApiDO = c.env.HIRO_API.get(id);
+    const response = await hiroApiDO.fetch(c.req.url);
+    return new Response(response.body, response);
+  } catch (error) {
+    return c.json(
+      {
+        error:
+          error instanceof Error
+            ? error.message
+            : `Unknown error ${String(error)}`,
+        status: 500,
+      },
       500
     );
   }
