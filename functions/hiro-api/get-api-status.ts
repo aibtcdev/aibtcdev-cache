a534f5d2dd24dfa159b7f2d2b3e04f4023b2de27
Jason Schrader <whoabuddy@users.noreply.github.com>
2024-11-07 17:44:13 -0700

    fix: correct some errors

--- functions/hiro-api/get-api-status.ts
+++ functions/hiro-api/get-api-status.ts
@@ -1,8 +1,8 @@
-import { HiroApiDO } from "../../src/durable-objects/hiro-api-do";
+import { PagesFunction } from "@cloudflare/workers-types";
+import { CloudflareBindings } from "../../worker-configuration";
 
 export const onRequest: PagesFunction<CloudflareBindings> = async (context) => {
   const id = context.env.HIRO_API.idFromName("hiro-api");
   const hiroApi = context.env.HIRO_API.get(id);
-  
   return hiroApi.fetch(context.request);
 };
