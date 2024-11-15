2800b641a89bd2013521d98ed008d4c29bcbb5f6
Jason Schrader <whoabuddy@users.noreply.github.com>
2024-11-15 01:02:31 -0700

    fix: set fetch opts, start extracting data

--- src/durable-objects/bns-do.ts
+++ src/durable-objects/bns-do.ts
@@ -3,7 +3,6 @@ import { Env } from '../../worker-configuration';
 import { AppConfig } from '../config';
 import { createJsonResponse } from '../utils/requests-responses';
 import { getKnownAddresses } from '../utils/address-store';
-import { RateLimitedFetcher } from '../rate-limiter';
 import { getNameFromAddress } from '../utils/bns-v2';
 import { validateStacksAddress } from '@stacks/transactions';
 
--- src/utils/bns-v2.ts
+++ src/utils/bns-v2.ts
@@ -1,3 +1,4 @@
+import { getFetchOptions, setFetchOptions } from '@stacks/common';
 import { BufferCV, ClarityType, fetchCallReadOnlyFunction, principalCV, TupleCV } from '@stacks/transactions';
 
 const BNS_CONTRACT_ADDRESS = 'SP2QEZ06AGJ3RKJPBV14SY1V5BBFNAW33D96YPGZF';
@@ -10,6 +11,15 @@ type NameResponse = {
 	namespace: BufferCV;
 };
 
+// workaround for using stacks.js fetch in Cloudflare Workers
+type StacksRequestInit = RequestInit & {
+	referrerPolicy?: string;
+};
+const fetchOptions: StacksRequestInit = getFetchOptions();
+console.log(`fetchOptions: ${JSON.stringify(fetchOptions)}`);
+delete fetchOptions.referrerPolicy;
+setFetchOptions(fetchOptions);
+
 function hexToAscii(bytes: Uint8Array) {
 	let str = '';
 	for (let n = 0; n < bytes.length; n += 1) {
@@ -19,24 +29,33 @@ function hexToAscii(bytes: Uint8Array) {
 }
 
 export async function getNameFromAddress(address: string, network: ValidNetworks = 'mainnet'): Promise<string> {
-	const addressCV = principalCV(address);
-	const response = await fetchCallReadOnlyFunction({
-		contractAddress: BNS_CONTRACT_ADDRESS,
-		contractName: BNS_CONTRACT_NAME,
-		functionName: 'get-name',
-		functionArgs: [addressCV],
-		senderAddress: address,
-		network: network,
-	});
-	if (response.type === ClarityType.ResponseErr) {
-		throw new Error(`Failed to get name for address ${address}`);
+	try {
+		const addressCV = principalCV(address);
+		const response = await fetchCallReadOnlyFunction({
+			contractAddress: BNS_CONTRACT_ADDRESS,
+			contractName: BNS_CONTRACT_NAME,
+			functionName: 'get-primary',
+			functionArgs: [addressCV],
+			senderAddress: address,
+			network: network,
+		});
+		if (response.type === ClarityType.ResponseErr) {
+			throw new Error(`Failed to get name for address ${address}`);
+		}
+		if (response.type === ClarityType.ResponseOk) {
+			const nameResponse = response.value as TupleCV<NameResponse>;
+			const { name, namespace } = nameResponse.value;
+			const nameStr = name.value;
+			const namespaceStr = namespace.value;
+			return `${nameStr}.${namespaceStr}`;
+		}
+		console.log(`response: ${JSON.stringify(response)}`);
+		throw new Error('getNameFromAddress: unexpected response type');
+	} catch (error) {
+		console.log(`getNameFromAddress error: ${error}`);
+		if (error instanceof Error) {
+			throw error;
+		}
+		throw new Error(String(error));
 	}
-	if (response.type === ClarityType.ResponseOk) {
-		const nameResponse = response.value as TupleCV<NameResponse>;
-		const { name, namespace } = nameResponse.value;
-		const nameStr = name.value;
-		const namespaceStr = namespace.value;
-		return `${nameStr}.${namespaceStr}`;
-	}
-	throw new Error('getNameFromAddress: unexpected response type');
 }
